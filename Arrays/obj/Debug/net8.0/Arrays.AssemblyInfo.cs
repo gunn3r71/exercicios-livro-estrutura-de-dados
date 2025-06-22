32b65ff5b8b81077e8cab4088f37f703d3cd5dc9
Lucas pereira <lucasgunner3@gmail.com>
2025-06-22 11:08:53 -0300

    fix: removendo bin e debug

--- Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c740d1099bc791e5818b3b113a41ba84a6532aa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595daa4f0e98f6cbff7218c611fdca1ada1c9843")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
